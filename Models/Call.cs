c6b0d81a0ec44cd88570f48df2a8320d5ff9c165
pw94 <pawelmwinnicki@gmail.com>
2018-01-07 22:22:49 +0100

    Added next call prediction

--- Models/Call.cs
+++ Models/Call.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace CallsCRM.Models
 {
     public class Call
@@ -8,5 +10,18 @@ namespace CallsCRM.Models
         public Customer Callee { get; set; }
         public int CallerId { get; set; }
         public Caller Caller { get; set; }
+
+        public DateTime PredictNext()
+        {
+            var time = Time.StartTime.AddDaysAfterNow();
+            if (time.IsWeekend())
+                time = time.AddDays(2);
+            else if (time.IsMorning())
+                time = time.AddHours(7);
+            else
+                time = time.AddHours(-7).AddDaysToSaturday();
+
+            return time;
+        }
     }
 }
---
+++ Models/DateTimeExtensions.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace CallsCRM.Models
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime TrimMilliseconds(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, 0);
+        }
+
+        public static bool IsWeekend(this DateTime dt)
+        {
+            return dt.DayOfWeek == DayOfWeek.Saturday
+                || dt.DayOfWeek == DayOfWeek.Sunday;
+        }
+
+        public static bool IsMorning(this DateTime dt)
+        {
+            return dt.Hour < 15;
+        }
+
+        public static DateTime AddDaysAfterNow(this DateTime dt)
+        {
+            var diff = (DateTime.Now - dt).TotalDays;
+            var date = dt.AddDays(diff+1);
+            if (dt.IsWeekend())
+                date = date.AddDaysToSaturday();
+            else
+                date = date.AddDays(2);
+            
+            return date;
+        }
+
+        public static DateTime AddDaysToSaturday(this DateTime dt)
+        {
+            var diff = DayOfWeek.Saturday - dt.DayOfWeek;
+            return dt.AddDays(diff);
+        }
+    }
+}
