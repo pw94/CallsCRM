43a5ba77b05df19604639cae967d5f338d605831
pw94 <pawelmwinnicki@gmail.com>
2018-01-11 12:51:41 +0100

    Fixed call defaults

--- Models/Call.cs
+++ Models/Call.cs
@@ -5,7 +5,7 @@ namespace CallsCRM.Models
     public class Call
     {
         public int CallId { get; set; }
-        public CallTime Time { get; set; }
+        public CallTime Time { get; set; } = new CallTime();
         public int CustomerId { get; set; }
         public Customer Callee { get; set; }
         public int CallerId { get; set; }
